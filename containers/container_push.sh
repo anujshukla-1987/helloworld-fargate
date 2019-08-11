1cce6b81c60ae4d45b90254818706e24da1af280
anujshukla-1987 <53939841+anujshukla-1987@users.noreply.github.com>
2019-08-11 18:09:50 +0200

    Add files via upload
    Changes

--- containers/container_push.sh
+++ containers/container_push.sh
@@ -1,8 +1,7 @@
 # Create ECR (if not already existing)
 aws ecr create-repository --repository-name "service-helloworld-api"
 
-ACCOUNT_ID=$(aws sts get-caller-identity |  jq -r '.Account')
-#ACCOUNT_ID="YOUR ACCOUNT ID"
+ACCOUNT_ID=$(aws sts get-caller-identity |  ./jq-win64.exe -r '.Account')
 $(aws ecr get-login --no-include-email --region us-east-1)
 
 docker build -t service-helloworld-api ./helloworld-api/
